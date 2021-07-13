21bc2626475203185ce8f8f23843ec1654a1dc9e
SangWon7242 <gotkdqja@gmail.com>
2021-07-13 12:22:00 +0900

    aboutMe HTML, CSS 디자인 작업

--- src/view/usr/home/aboutMe.php
+++ src/view/usr/home/aboutMe.php
@@ -7,22 +7,34 @@ $pageTitle = "주인장소개";
 
 <section class="section-about-me">
 	<div class="container mx-auto">
-			<div class="con-pad">
-			자기소개 페이지 조만간 업로드 예정
-			<ul>
-				<li>
-					<a href="https://github.com/SangWon7242" target="_blank" class="h-full flex items-center px-4 hover:underline">
-						<span><i class="fab fa-github"></i></span>
-						<span class="font-bold hidden sm:block">&nbsp;GITHUB</span>
-					</a>
-				</li>
-				<li>
-				 <a href="https://www.youtube.com/user/gotkdqja/videos" target="_blank" class="h-full flex items-center px-4 hover:underline">
-					 <span><i class="fab fa-youtube"></i></span>
-						<span class="font-bold hidden sm:block">&nbsp;YOUTUBE</span>
-					</a>
-				</li>
-			</ul>
+		<div class="con-pad">
+			<div class="frame">
+  <div class="dashboard">
+    <h1 class="korean-comment" style="line-height:150%">안녕하세요! 제 이름은 <a class="sample">코린이</a><br><a class="highlight type1">진짜 문제</a>를 <a class="highlight type2">제대로</a> 해결하는 <a class="highlight type3">풀스텍 개발자를</a><br>를 꿈꿉니다.</h1>
+
+    <p class="english-comment">Just Do It! Don't Give Up!</p>
+  </div>
+
+  <h5 class="recent-menu">RECENT WORKS</h5>
+
+  <div class="recent-works">
+
+  </div>
+  <footer class="footer">
+    <h3 class="contact">CONTACT ME</h3>
+    <div class="links">
+      <a href="https://github.com/SangWon7242" target="_blank" class="h-full flex items-center px-4 hover:underline">
+        <span><i class="fab fa-github"></i></span>
+        <span class="font-bold hidden sm:block">&nbsp;GITHUB</span>
+      </a>
+      <a href="https://www.youtube.com/user/gotkdqja/videos" target="_blank" class="h-full flex items-center px-4 hover:underline">
+        <span><i class="fab fa-youtube"></i></span>
+        <span class="font-bold hidden sm:block">&nbsp;YOUTUBE</span>
+      </a>
+    </div>
+  </footer>
+
+</div>
 		</div>
 	</div>
 </section>
